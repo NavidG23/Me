d96d855caa80350c61cdb5bc3061f617ab4469e0
Navid Galt <ngalt@deloitte.com>
2019-10-07 21:28:12 -0400

    UI facelift of website

---
+++ js/script.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+  document.getElementById("homeTab").style.backgroundColor = 'black';
+}
+
+function tabActive(elmt) {
+	document.getElementById("homeTab").style.backgroundColor = '#222';
+	document.getElementById("aboutTab").style.backgroundColor = '#222';
+	document.getElementById("photosTab").style.backgroundColor = '#222';
+	document.getElementById("contactTab").style.backgroundColor = '#222';
+	elmt.style.backgroundColor = 'black';
+}
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+$(document.body).on('touchmove', onScroll);
+$(window).on('scroll', onScroll); 
+
+function onScroll() {
+	var pos = window.scrollY;
+
+	// Link Highlighting
+	if (pos > 0 && pos < 598) {
+		tabActive(document.getElementById("homeTab"), 'homeTab');
+	}
+	if (pos > 599 && pos < 1348) {
+		tabActive(document.getElementById("aboutTab"), 'aboutTab');
+	}
+	if (pos > 1349 && pos < 2270) {
+		tabActive(document.getElementById("photosTab"), 'photosTab');
+	}
+	if (pos > 2271 && pos < 2351) {
+		tabActive(document.getElementById("contactTab"), 'contactTab');
+	}
+}
+
+function highlightLink(anchor) {
+	$('nav .active').removeClass('active');
+	$("nav").find('[dest="' + anchor + '"]').addClass('active');
+}
